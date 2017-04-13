08ee20a79f834148962062b6181717027b14f100
Mukund Kidambi <mukkid@gmail.com>
2017-04-13 00:26:03 -0500

    added INC (increment) function

--- src/ops.c
+++ src/ops.c
@@ -21,6 +21,23 @@ int exec_adc_common(struct nes_state *state, uint8_t operand, int pc_increment) 
     return 0;
 }
 
+int exec_inc_common(struct nes_state *state, uint16_t operand, int pc_increment){
+	
+	state->regs.PC += pc_increment;
+
+	/* Increment Memory by One: INC */
+	/* M + 1 -> M */
+	uint16_t address = operand;
+	/* increment value at memory */
+	state->ram[address]++;
+
+	/* set negative flag if bit 7 is set */
+	state->regs.SR.N = ((state->ram[address]) & 0x80) >> 7;
+	/* set zero flag */
+	state->regs.SR.Z = (state->ram[address]) == 0;
+	return 0;
+}
+
 int exec_adc_imm(struct nes_state *state) {
     return exec_adc_common(state, state->rom[state->regs.PC + 1], 2);
 }
